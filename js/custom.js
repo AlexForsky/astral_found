3582ce6ede19dda4a0e21514a241a6374ea5d625
AlexForsky <alex.forsky@gmail.com>
2017-03-26 23:56:08 +0400

    tarif_ao1

--- js/custom.js
+++ js/custom.js
@@ -3,6 +3,8 @@ $('.menu_baz').find('li.baz').click(function() {
   $(this).addClass('active');
   console.log($(this).data('prise'));
   var prise = $(this).data('prise-baz');
+  var priseDop = $(this).data('prise-baz-dop');
   console.log(prise);
   $('.data-price-baz_text').text(prise);
+  $('.data-price-baz_text_dop').text(priseDop);
 })
