37a5880cce85151eb5e941fbbc2508a6fc1ee9a6
AlexForsky <alex.forsky@gmail.com>
2017-03-26 23:25:56 +0400

    tarif-ao

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,8 @@
-$('.prav_form').find('li').click(function() {
-  $('.prav_form .active').removeClass('active');
+$('.menu_baz').find('li.baz').click(function() {
+  $('.menu_baz .active').removeClass('active');
   $(this).addClass('active');
   console.log($(this).data('prise'));
-  var prise = $(this).data('prise');
+  var prise = $(this).data('prise-baz');
   console.log(prise);
-  $('.data-price-text').text(prise);
+  $('.data-price-baz_text').text(prise);
 })
