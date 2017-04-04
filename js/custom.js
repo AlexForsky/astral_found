9993a0f6c86df1ba02970100026fff6100064517
AlexForsky <alex.forsky@gmail.com>
2017-04-04 15:04:07 +0300

    tarif-js

--- js/custom.js
+++ js/custom.js
@@ -27,38 +27,6 @@ $('.menu_group').find('li.group').click(function() {
  })
 
 
-//для базового Москва
-$('.menu_baz_m').find('li.baz_m').click(function() {
-  $('.menu_baz_m .active').removeClass('active');
-  $(this).addClass('active');
-  var prise = $(this).data('prise-baz_m');
-  var priseDop = $(this).data('prise-baz_m-dop');
-  $('.data-price-baz_m_text').text(prise);
-  $('.data-price-baz_m_text_dop').text(priseDop);
-})
-
-//для оптимального Москва
-$('.menu_opt_m').find('li.opt_m').click(function() {
-  $('.menu_opt_m .active').removeClass('active');
-  $(this).addClass('active');
-  var prise = $(this).data('prise-opt_m');
-  var priseDop = $(this).data('prise-opt_m-dop');
-  $('.data-price-opt_m_text').text(prise);
-  $('.data-price-opt_m_text_dop').text(priseDop);
-})
-
-//для ГК Москва
-$('.menu_group_m').find('li.group_m').click(function() {
-  $('.menu_group_m .active').removeClass('active');
-  $(this).addClass('active');
-  var prise = $(this).data('prise-group_m');
-  $('.data-price-group_m_text').text(prise);
- })
 
 
-//обработчик меню тарифов
-$('.tarif__menu').find('li.tarif__menu-item').click(function() {
-  $('.accordion-item .is-active').removeClass('is-active');
-  
- })
 
