70b6bbe1d3c92bc2d85f33cb5cccb5ae447769c4
AlexForsky <alex.forsky@gmail.com>
2017-09-29 17:09:03 +0300

    service_form1

---
+++ php/service_send.php
@@ -0,0 +1,8 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv='refresh' content='7; url=http://astral.of/service.html'></meta>
+</head>    
+<p align="center">Ваше сообщение было успешно отправлено!</p>
+<p align="center">Если сегодня рабочий день, то в ближайшее время с вами свяжутся.</p>
+<p align="center">Если сегодня выходной, то с вами свяжутся в ближайший рабочий день.</p>
+<p align="center"><a href="../service.html">Вернуться обратно</a></p>
