503440747399494d67e3edbf2718d1b0456b5048
AlexForsky <alex.forsky@gmail.com>
2017-10-03 15:47:29 +0300

    service_form2

--- php/service_send.php
+++ php/service_send.php
@@ -1,3 +1,43 @@
+<?php
+ 
+/* Задаем переменные */
+$name = htmlspecialchars($_POST["service__form_name"]);
+$email = htmlspecialchars($_POST["service__form_email"]);
+$tel = htmlspecialchars($_POST["service__form_phone"]);
+$message = htmlspecialchars($_POST["service__form_message"]);
+$bezspama = htmlspecialchars($_POST["bezspama"]);
+ 
+/* Ваш адрес и тема сообщения */
+$address = "support@gk-soft.ru";
+$sub = "Заказ сопровождения";
+ 
+/* Формат письма */
+$mes = "Заказ сопровождения с сайта.\n
+Имя отправителя: $name 
+Электронный адрес отправителя: $email
+Телефон отправителя: $tel
+Текст сообщения:
+$message";
+ 
+ 
+if (empty($bezspama)) /* Оценка поля bezspama - должно быть пустым*/
+{
+/* Отправляем сообщение, используя mail() функцию */
+$from  = "From: $name <$email> \r\n Reply-To: $email \r\n";
+if (mail($address, $sub, $mes, $from)) {
+ header('Refresh: 5; URL=http://astral.of/service.html');
+ echo '<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+    <body>Письмо отправлено, через 5 секунд вы вернетесь на страницу </body>';}
+else {
+ header('Refresh: 5; URL=http://astral.of/service.html');
+ echo '<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+    <body>Письмо не отправлено, через 5 секунд вы вернетесь на страницу YYY</body>';}
+}
+exit; /* Выход без сообщения, если поле bezspama заполнено спам ботами */
+?>
+<!--
 <head>
     <meta charset="utf-8">
     <meta http-equiv='refresh' content='7; url=http://astral.of/service.html'></meta>
@@ -6,3 +46,4 @@
 <p align="center">Если сегодня рабочий день, то в ближайшее время с вами свяжутся.</p>
 <p align="center">Если сегодня выходной, то с вами свяжутся в ближайший рабочий день.</p>
 <p align="center"><a href="../service.html">Вернуться обратно</a></p>
+-->
